670020ffb8204c5b87acc94d74afcdbc5215a628
Michael Levesque <mlevesque@Michaels-MacBook-Pro.local>
2017-07-07 17:51:15 -0700

    Added JSON files and a JSON Loader helper class.

---
+++ CocosTest/Classes/JSONLoader.cpp
@@ -0,0 +1,39 @@
+//
+//  JSONLoader.cpp
+//  CocosTest
+//
+//  Created by Michael Levesque on 7/7/17.
+//
+//
+
+#include "JSONLoader.h"
+#include <fstream>
+#include <string>
+
+/**
+ * Constructor. We will try to load the file and parse it into the document object.
+ */
+JSONLoader::JSONLoader (const char* filename)
+: m_filename(filename) {
+    Reload();
+}
+
+/**
+ * Reloads the file and re-parses it.
+ */
+void JSONLoader::Reload (void) {
+    // load file and put into a string
+    std::ifstream infile (m_filename);
+    std::string file_contents {std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>()};
+    infile.close();
+    
+    // parse json
+    m_document.Parse(file_contents.c_str());
+}
+
+/**
+ * Returns a reference to the document object. This object contains the parsed JSON data.
+ */
+rapidjson::Document& JSONLoader::GetDocument (void) {
+    return m_document;
+}
---
+++ CocosTest/Classes/JSONLoader.h
@@ -0,0 +1,35 @@
+//
+//  JSONLoader.h
+//  CocosTest
+//
+//  Created by Michael Levesque on 7/7/17.
+//
+//
+
+#ifndef JSONLoader_h
+#define JSONLoader_h
+
+#include "document.h" // for rapidjson::Document
+
+/**
+ * JSONLoader loads a given json file and uses Rapid JSON to to parse it.
+ */
+class JSONLoader {
+private:
+    // store filename so it can be reloaded whenever
+    const char* m_filename;
+    
+    // this is the Rapid JSON object that the file gets parsed into
+    rapidjson::Document m_document;
+public:
+    // Constructor. This will start loading and parsing the given file.
+    JSONLoader (const char* filename);
+    
+    // will reopen the file and reparse it.
+    void Reload (void);
+    
+    // returns the Rapid JSON parsed object.
+    rapidjson::Document& GetDocument (void);
+};
+
+#endif /* JSONLoader_h */
